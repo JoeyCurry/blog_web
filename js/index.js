d8b1a2ef137cf5d5acdf53428099f5a82d84ef96
jiangjun <jiangjun_89125@126.com>
2016-11-20 22:15:03 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var BASE_URL = "http://192.168.1.3:8000/";
+var REGISTER_URL = BASE_URL + "action/register";
+var RECORD_URL = BASE_URL + "action/record";
+var ASKDOCTOR_URL = BASE_URL + "action/chat";
+var BASEMSG_URL = BASE_URL + "action/baseinfo";
+
+// $(document).ready(function(){
+//     registerPost();
+// });
---
+++ js/register.js
@@ -0,0 +1,10 @@
+var BASE_URL = "http://192.168.1.3:8000/";
+
+$(document).ready(function(){
+
+});
+
+function register(){
+    console.log('register');
+    //window.location.href="file:///Users/jiang/workspace/blog_web/index.html";
+}
